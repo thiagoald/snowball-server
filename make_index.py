e9f30bb2c3b9c134ee2500b9de9230bfafa534c2
Thiago Domingues <thiagoald@gmail.com>
2021-04-29 18:19:23 -0300

    Fix race conditions

--- make_index.py
+++ make_index.py
@@ -8,26 +8,25 @@ from multiprocessing import Pool, Manager, Value
 
 manager = Manager()
 vars = manager.list()
-new_var_idx = manager
 
 
 def write_to_file(jsons_filepath):
     head, tail = path.split(jsons_filepath)
-    index_filepath = f"{tail}_.index"
-    print(f"Including {jsons_filepath}...")
+    index_filepath = f"{tail}.index"
+    print(f"Including {jsons_filepath}... len(vars)={len(vars)}")
     try:
         if (not os.path.exists(index_filepath)):
-            head_idx = find_idx(vars, head)
-            tail_idx = find_idx(vars, tail)
-            if (head_idx is None):
-                add_to_vars(vars, vars_filepath, new_var_idx.value, head)
-                head_idx = new_var_idx.value
-                new_var_idx.value += 1
-            if (tail_idx is None):
-                add_to_vars(vars, vars_filepath, new_var_idx.value, tail)
-                tail_idx = new_var_idx.value
-                new_var_idx.value += 1
-            with open(index_filepath, "a") as f_index:
+            try:
+                head_idx = vars.index(head)
+            except ValueError:
+                vars.append(head)
+                head_idx = vars.index(head)
+            try:
+                tail_idx = vars.index(tail)
+            except ValueError:
+                vars.append(tail)
+                tail_idx = vars.index(tail)
+            with open(index_filepath, "w") as f_index:
                 with open(jsons_filepath) as f:
                     while True:
                         start = f.tell()
@@ -35,7 +34,6 @@ def write_to_file(jsons_filepath):
                         if (len(line) == 0):
                             break
                         paperId = json.loads(line)["id"]
-                        end = f.tell()
                         f_index.write(
                             f"{paperId},{head_idx}/{tail_idx},{start}\n")
         else:
@@ -57,8 +55,12 @@ if __name__ == "__main__":
         vars = Manager().list(vars_)
     else:
         vars, max_idx = Manager().list(), 0
-    new_var_idx = Value('i', max_idx + 1)
     filepaths = [f for f in sys.argv[4:] if 's2-corpus-' in f]
     with Pool(processes) as p:
         p.map(write_to_file, [filepaths[i]
                               for i in range(len(filepaths))])
+    for filepath in filepaths:
+        write_to_file(filepath)
+    with open(vars_filepath, "w") as f:
+        for i, var in enumerate(vars):
+            f.write(f"{i},{var}\n")
